11fee6a904e76832f8a88bdc0be42bbd03e9a06b
Carnitter <rottor467@gmail.com>
2016-10-07 10:44:58 +0200

    the fix
    fix that shit

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        List<Player> plays = new List<Player>();
+
+        public Game()
+        {
+
+        }
+
+        public void update(Player play)
+        {
+            plays.Add(play);
+            hasWon();
+
+        }
+
+        public int hasWon()
+        {
+            
+
+        }
+
+
+        
+    }
+}
